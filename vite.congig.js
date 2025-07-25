3f79efe4c9b4463f3bedd5810761bad04eecbf46
ShyamalWarnakula <122584532+Warnakulaisuru@users.noreply.github.com>
2025-07-25 12:24:20 +0530

    Update vite.congig.js

--- vite.congig.js
+++ vite.congig.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // No sub-path, this runs directly at root: debtx.slt.lk
+  base: '/test/', // No sub-path, this runs directly at root: debtx.slt.lk
 })
