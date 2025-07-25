f6d47ec345f9a79debeaef882b0ad262fdd62a2d
ShyamalWarnakula <122584532+Warnakulaisuru@users.noreply.github.com>
2025-07-25 12:31:06 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <BrowserRouter basename="/test">
+      <Routes>
+        <App />
+      </Routes>
+    </BrowserRouter>
+  );
+}
